7ac5f323feaaae39b9780bc22cfa5a54edb5c79f
Prateek <workspace.infoga@gmail.com>
2023-09-30 00:20:36 +0530

    DSA

--- cw/Day007/p01.cpp
+++ cw/Day007/p01.cpp
@@ -8,45 +8,49 @@ int main()
     cout << "Enter length of each traingles : ";
     cin >> n;
 
+    for (int row = 0; row < n; row++)
+    {
+        for (int col = 0; col < (n - row - 1); col++)
+        {
+            cout << " ";
+        }
+        for (int col = 0; col < (2*(row) + 1); col++)
+        {
+            if (col == 0)
+            {
+                cout << "*";
+            }
+            else if (col == 2*row)
+            {
+                cout << "*";
+            }
+            else
+            {
+                cout << " ";
+            }
+        }  
+        cout << endl;
+    }
+    // int m = n;
     // for (int row = 0; row < n; row++)
     // {
-    //     for (int col = 0; col < (n - row); col++)
+    //     for (int col = 0; col < row; col++)
     //     {
     //         cout << " ";
     //     }
-    //     for (int col = 0; col < (2*(row) + 1); col++)
+    //     for (int col = 0; col < ((2*(n) - 2*(row)) - 1); col++)
     //     {
-    //         if (col == 0 || col == 2*row)
+    //         if ( col == 0 || col == 2*(n-1) )
     //         {
     //             cout << "* ";
     //         }
     //         else
     //         {
-    //             cout << " ";
+    //             cout << ".";
     //         }
+    //         // m =  m - 1;
     //     }  
     //     cout << endl;
     // }
-    int m = n;
-    for (int row = 0; row < n; row++)
-    {
-        for (int col = 0; col < row; col++)
-        {
-            cout << " ";
-        }
-        for (int col = 0; col < ((2*(n) - 2*(row)) - 1); col++)
-        {
-            if ( col == 0 || col == 2*(n-1) )
-            {
-                cout << "* ";
-            }
-            else
-            {
-                cout << ".";
-            }
-            // m =  m - 1;
-        }  
-        cout << endl;
-    }
     
 }
