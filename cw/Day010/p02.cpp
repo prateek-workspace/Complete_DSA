99f5d36ec80d32b72a7ec699a9d326536f8dc59c
Prateek <workspace.infoga@gmail.com>
2023-10-01 20:00:12 +0530

    DSA

---
+++ cw/Day010/p02.cpp
@@ -0,0 +1,11 @@
+// WAP to print flipped solid diamond 
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter length of each traingles : ";
+    cin >> n;
+    
+}
