d50fe30ab166f9c08f3876cd20cf907bd8482b72
Prateek <workspace.infoga@gmail.com>
2023-10-01 20:24:21 +0530

    DSA

--- cw/Day010/p02.cpp
+++ cw/Day010/p02.cpp
@@ -5,7 +5,56 @@ using namespace std;
 int main()
 {
     int n;
-    cout << "Enter length of each traingles : ";
+    cout << "Enter length of characters : ";
     cin >> n;
+
+    int m = n / 2;
+
+    int row,col = 0;
     
+    for (row = 0; row < m; row++)
+    {
+        for (col = 0; col <m-row ; col++)
+        {
+            cout << "*";
+        }
+
+
+        for (col = 0; col < 2*(row); col++)
+        {
+            cout << " ";
+        }
+
+
+        for (col = 0; col < m-row; col++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+
+    for (row = 0; row < m; row++)
+    {
+        for (col = 0; col <row+1 ; col++)
+        {
+            cout << "*";
+        }
+
+        for (col = 0; col < n - 2*(row+1); col++)
+        {
+            cout << " ";
+        }
+
+        for (col = 0; col < row + 1; col++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+
+    
+
+
 }
