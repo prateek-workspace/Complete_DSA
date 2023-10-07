ac4ff0a4065adf6f48f3500931d07891b5a0453a
Prateek <workspace.infoga@gmail.com>
2023-10-07 13:18:27 +0530

    DSA

---
+++ cw/Day015/p01.cpp
@@ -0,0 +1,15 @@
+// WAP for break statement.
+
+#include<iostream>
+using namespace std;
+int main()
+{
+
+    for (int i = 0; i < 4; i++)
+    {
+        /* code */
+        cout << "Prateek ";
+        break;
+    }
+    cout << "Srivastava";    
+}
---
+++ cw/Day015/p02.cpp
@@ -0,0 +1,17 @@
+// WAP for continue statement.
+
+#include<iostream>
+using namespace std;
+int main()
+{
+
+    for (int i = 0; i < 5; i++)
+    {
+        /* code */
+        continue;
+        cout << i << " Hello\n";
+        
+    }
+    
+    
+}
