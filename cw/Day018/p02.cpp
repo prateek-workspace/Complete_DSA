3a011e555263479c312d2425e94cf53638db8ca0
Prateek <workspace.infoga@gmail.com>
2023-10-10 07:29:00 +0530

    DSA

--- cw/Day018/p02.cpp
+++ cw/Day018/p02.cpp
@@ -1,10 +1,20 @@
-// WAP to 
+// WAP to comvert miles to km.
 
 #include<iostream>
 using namespace std;
+
+float milesToKm(float n)
+{
+    float km = 1.6*n;
+    return (km);
+}
+
 int main()
 {
+    cout << "Enter Distance in Miles : ";
+    int n;
+    cin >> n;
 
-    
-    
+    cout << milesToKm(n);
+        
 }
