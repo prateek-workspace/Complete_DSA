05132edf93684831a6a38f57903e14e2d067a115
Prateek <workspace.infoga@gmail.com>
2023-10-10 22:14:54 +0530

    DSA

---
+++ cw/Day019/p01.cpp
@@ -0,0 +1,26 @@
+// WAP to demonstrate arrays. 
+
+#include<iostream>
+using namespace std;
+int main()
+{
+
+    // int arr[53];
+    // cout << arr << endl;
+
+    // char newarr[106];
+    // cout <<newarr<< endl;
+
+    // bool boolarr[23];
+    // cout << boolarr << endl;
+   
+    int aarr[]={1,2,3,4,5,6,7,8,9,0};
+    int brr[5]={2,4,6,8,0};
+    // if we give less elements than the initialised space then remainig will contain 0
+    int crr[10]={2,4,6,8,0};
+    // if we provide more elements and less space then it may shows error
+    int drr[8]={9,8,7,6};
+    cout << drr << endl;
+    cout << "hii";
+    
+}
