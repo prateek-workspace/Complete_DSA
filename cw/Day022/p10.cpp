3b4e6bc68fcb4f96b98490e3cebe45327464cb97
Prateek <workspace.infoga@gmail.com>
2023-10-14 11:52:45 +0530

    DSA

--- cw/Day022/p10.cpp
+++ cw/Day022/p10.cpp
@@ -1,26 +1,26 @@
 // WAP to reverse an array.  
 
-#include<iostream>
-using namespace std;
-int main()
-{
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
 
-    int arr[9]={1,3,5,7,9,2,4,6,8};
-    int size = 8;
+//     int arr[9]={1,3,5,7,9,2,4,6,8};
+//     int size = 8;
 
-    for (int i = 0; i < 9; i++)
-    {
-        int count = size - i;
-        int b = arr[count];
-        // cout << b << " ";
-        int brr[9];
-        brr[i] = b;
-        cout << brr[i] << " ";
+//     for (int i = 0; i < 9; i++)
+//     {
+//         int count = size - i;
+//         int b = arr[count];
+//         // cout << b << " ";
+//         int brr[9];
+//         brr[i] = b;
+//         cout << brr[i] << " ";
+//     }
 
-    }
+// }
 
-}
-
+// Alternate Method 
 
 // #include<iostream>
 // using namespace std;
@@ -44,3 +44,29 @@ int main()
 //     }
 
 // }
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[9]={1,3,5,7,9,2,4,6,8};
+    int size = 9;
+    int start = 0;
+    int end = size - 1;
+    while(start<=end)
+    {
+        // swap(arr[start],arr[end]); 
+        // start++;
+        // end--;
+
+        arr[end] =  arr[end] - arr[start];
+        arr[start] = arr[start] + arr[end];
+        arr[end] = arr[start] - arr[end];
+
+    }
+
+    for (int i = 0; i < 9; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
