beccbd64be0c736a0618a3a40b22c422f84873f1
Prateek <workspace.infoga@gmail.com>
2023-10-14 21:54:24 +0530

    DSA

--- cw/Day023/p01.cpp
+++ cw/Day023/p01.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 int main()
 {
-
+    // first way 
     vector<int> arr;
 
     arr.push_back(5);
@@ -21,7 +21,14 @@ int main()
         cout << arr[i] << endl;
     }
 
-    vector<int> mine(10,6);
+    // second way 
+
+    int n;
+    cout << "Enter the size of vector you want : ";
+    cin >> n;
+
+    vector<int> mine(n,6);
+    cout << "Is mine empty or not : " << mine.empty() << endl;
     cout << "Size of mine is : " << mine.size() << endl;
 
         // printing 
@@ -29,6 +36,18 @@ int main()
     for (int i = 0; i < mine.size(); i++)
     {
         cout << mine[i] << endl;
+    } 
+
+    // third way (less use)
+
+    vector<int> brr{10,20,30,40,50};
+    cout << "Size of brr is : " << brr.size() << endl;
+
+        // printing 
+
+    for (int i = 0; i < brr.size(); i++)
+    {
+        cout << brr[i] << endl;
     } 
     
     
