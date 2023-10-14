16022e6f3a03c8b200fe8c8d7fbca9c749bc385f
Prateek <workspace.infoga@gmail.com>
2023-10-14 09:28:18 +0530

    DSA

--- cw/Day022/p09.cpp
+++ cw/Day022/p09.cpp
@@ -5,16 +5,20 @@ using namespace std;
 int main()
 {
 
-    int arr[8]={1,3,5,7,9,2,4,6};
-    int size = 7;
+    int arr[9]={1,3,5,7,9,2,4,6,8};
+    int size = 8;
 
-    for (int i = 0; i < 8; i++)
+    for (int i = 0; i < 9; i++)
     {
         int count = size - i;
         if (i<count)
         {
         cout << arr[i] << " ";
         cout << arr[count] << " "; 
+        }
+        else if (i == count)
+        {
+            cout << arr[i] << " ";
         }
         
         
--- cw/Day022/p10.cpp
+++ cw/Day022/p10.cpp
@@ -5,19 +5,18 @@ using namespace std;
 int main()
 {
 
-    int arr[8]={1,3,5,7,9,2,4,6};
-    int size = 7;
+    int arr[9]={1,3,5,7,9,2,4,6,8};
+    int size = 8;
 
-    for (int i = 0; i < 8; i++)
+    for (int i = 0; i < 9; i++)
     {
         int count = size - i;
-        arr[i] = arr[count];
-    }
+        int b = arr[count];
+        // cout << b << " ";
+        int brr[9];
+        brr[i] = b;
+        cout << brr[i] << " ";
 
-    for (int i = 0; i < 8; i++)
-    {
-        cout << arr[i] << " ";
     }
-
 
 }
