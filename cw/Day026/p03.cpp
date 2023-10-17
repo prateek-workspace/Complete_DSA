640295e9596a2a48b29acbdb3a9dc1015ef70493
Prateek <workspace.infoga@gmail.com>
2023-10-17 23:23:10 +0530

    DSA

---
+++ cw/Day026/p03.cpp
@@ -0,0 +1,57 @@
+// WAP sort 0's and 1's in arrays.
+    // (Dutch National Flag Problem)
+
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+// int main()
+// {
+//     // given array
+//     vector<int> arr{0,1,1,0,1,0,0,1,0,1,1,1,1,0,1};
+//     vector<int> brr;
+
+//     // pushing 0's
+
+//     for (int i = 0; i < arr.size(); i++)
+//     {
+//         if (arr[i] == 0)
+//         {
+//             brr.push_back(arr[i]);
+//         } 
+//     }
+
+//     // pushing 1's
+
+//     for (int i = 0; i < arr.size(); i++)
+//     {
+//         if (arr[i] == 1)
+//         {
+//             brr.push_back(arr[i]);
+//         } 
+//     }
+
+//     // printing the sorted array.
+
+//     cout << "[";
+//     for (int i = 0; i < arr.size(); i++)
+//     {
+//         cout << brr[i] << ",";
+//     }
+//     cout << "]";
+
+//     return 0;
+// }
+
+
+// two pointer approach:----
+
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+    
+    
+    
+    return 0;
+}
