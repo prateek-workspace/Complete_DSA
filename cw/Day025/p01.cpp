5c1ce9a1d337a87890c8c2f65dfee8e47810f2b7
Prateek <workspace.infoga@gmail.com>
2023-10-17 21:25:25 +0530

    DSA

--- cw/Day025/p01.cpp
+++ cw/Day025/p01.cpp
@@ -5,8 +5,25 @@
 using namespace std;
 int main()
 {
-    
-    
-    
+    vector<int> arr{1,2,3,4,5,6,7,8,9};
+
+    // pair sum = 9
+
+    for (int i = 0; i < arr.size(); i++)
+    {
+        for (int j = i+1; j < arr.size(); j++)
+        {
+            if (arr[i] + arr[j] == 111)
+            {
+                cout << "(" << arr[i] << "," << arr[j] << ")" << endl;
+            }
+            // else
+            // {
+            //     cout << "Desired pair not present" << endl;
+            //     break;
+            // }  
+        }
+        
+    }  
     return 0;
 } 
