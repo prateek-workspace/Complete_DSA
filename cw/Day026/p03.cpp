d8efbb9a15218fae3c5875417e33c475586f98b5
Prateek <workspace.infoga@gmail.com>
2023-10-17 23:33:36 +0530

    DSA

--- cw/Day026/p03.cpp
+++ cw/Day026/p03.cpp
@@ -51,7 +51,37 @@ using namespace std;
 int main()
 {
     
+    vector<int> arr{0,1,1,0,1,0,0,1,0,1,1,1,1,0,1};
+    
+    int start = 0;
+    int i = 0;
+    int end = arr.size() - 1;
+
+    while (i!=end)
+    {
+        if (arr[i] == 0)
+        {
+            /* code */
+            swap(arr[start],arr[i]);
+            i++;
+            start++;
+        }
+
+        else
+        {
+            swap(arr[i],arr[end]);
+            end--;
+        }
+        
+    }
+
+    for (auto value : arr)
+    {
+        cout << value << "," ;
+    }
     
     
+    
+
     return 0;
 }
