d9ec8ae4385157fa8a94328d857f14f5630919a3
Prateek <workspace.infoga@gmail.com>
2023-10-18 22:36:41 +0530

    DSA

---
+++ cw/Day027/p01.cpp
@@ -0,0 +1,48 @@
+// WAP to demonstrate 2-D Arrays. 
+
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+    int n;
+    int m;
+    cout << "Enter no of rows : ";
+    cin >> n;
+
+    cout << "Enter no of columns : ";
+    cin >> m;
+
+    int arr[n][m];
+
+    // taking values as input
+
+    for (int i = 0; i < n; i++)
+    {
+        /* code */
+        cout << "Entering values for " << i << " row" << endl;
+        for (int j = 0; j < m; j++)
+        {
+        cout << "Enter values for " << j << " column" << endl;
+        cin >> arr[i][j];
+        }
+
+    }
+
+    // displaying output of 2D Array.
+    cout << "{";
+    for (int i = 0; i < n; i++)
+    {
+        cout << "{";
+        for (int j = 0; j < m; j++)
+        {
+            cout << arr[i][j] << ",";
+        }
+        cout << "},";
+        
+    }
+    cout << "}" << endl;
+    
+        
+    return 0;
+}
