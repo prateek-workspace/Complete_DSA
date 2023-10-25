77df23993c7eeb414ce66df2f67dcb967f182695
Prateek <workspace.infoga@gmail.com>
2023-10-25 23:59:16 +0530

    DSA

--- hw/Week02/p02.cpp
+++ hw/Week02/p02.cpp
@@ -1,3 +1,8 @@
+/**
+ * This C++ program prints Pascal's triangle with a user-defined number of rows.
+ * 
+ * @return The main function is returning an integer value of 0.
+ */
 
 #include<iostream>
 #include<vector>
@@ -9,15 +14,25 @@ int main()
     cout << "Enter number of rows : " << endl;
     cin >> n;
 
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i <= n; i++)
     {
+        int calc = 1;
+
         for (int j = 0; j < n-i; j++)
         {
-            cout << "* ";
+            cout << " ";
         }
+        
+        for (int j = 1; j <= i; j++)
+        {
+            cout << calc  << " ";
+            calc = calc*(i - j) / j;
+        }
+
         cout << endl;
+        
     }
     
-    
+
     return 0;
 }
