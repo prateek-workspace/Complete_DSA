9242c24e946b691039aadfbcbaaee291a8000a23
Prateek <workspace.infoga@gmail.com>
2023-10-25 23:50:04 +0530

    Updated p02.cpp

--- hw/Week02/p02.cpp
+++ hw/Week02/p02.cpp
@@ -9,6 +9,14 @@ int main()
     cout << "Enter number of rows : " << endl;
     cin >> n;
 
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n-i; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
     
     
     return 0;
