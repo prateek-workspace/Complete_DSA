27d7c1b2a6fd6488f032fa2a2692c7d33181059e
Prateek <workspace.infoga@gmail.com>
2023-11-11 23:40:01 +0530

    DSA

--- cw/Day033/p03.cpp
+++ cw/Day033/p03.cpp
@@ -2,10 +2,84 @@
 #include<iostream>
 #include<vector>
 using namespace std;
+int firstOccurrence(vector<int> arr, int num )
+{
+
+    int start, end, mid;
+    int ans = -1;
+    start = 0;
+    end = arr.size() - 1;
+
+    mid = start + ((end-start)/2); 
+
+    while (start <= end)
+    {
+        if (arr[mid] == num)
+        {   
+            ans = mid;
+            end = mid -1;
+        }
+        else if (arr[mid] > num)
+        {
+            end = mid - 1;
+        }
+        else if (arr[mid] < num)
+        {
+            start = mid + 1;
+        }  
+
+        mid = start + ((end-start)/2); 
+
+    }
+    return ans;
+}
+
+int lastOccurrence(vector<int> arr, int num )
+{
+
+    int start, end, mid;
+    int ans = -1;
+    start = 0;
+    end = arr.size() - 1;
+
+    mid = start + ((end-start)/2); 
+
+    while (start <= end)
+    {
+        if (arr[mid] == num)
+        {   
+            ans = mid;
+            start = mid + 1;
+        }
+        else if (num < arr[mid])
+        {
+            end = mid - 1;
+        }
+        else if (num > arr[mid])
+        {
+            start = mid + 1;
+        }  
+
+        mid = start + ((end-start)/2); 
+
+    }
+    return ans;
+}
+
 int main()
 {
-    
-    
-    
+
+    int num, size;    
+    vector<int> arr{0,1,3,4,5,6,6,6,7,8,9,9,9,9,9,9,9};
+    size = arr.size();
+    cout << "Enter the number that you want to search : " << endl;
+    cin >> num;
+    int ans = firstOccurrence(arr, num);
+    int ans2 = lastOccurrence(arr, num);
+
+    int total = ans2 - ans + 1;
+
+    cout << "Total Occurences are : " << total << endl;
+
     return 0;
 }
---
+++ cw/Day033/p04.cpp
@@ -0,0 +1,21 @@
+
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+    vector<int> arr = {1,2,3,4,5,6,8,9};
+    int size = arr.size();
+    int actualSum = ((arr[size]) * (arr[size] + 1))/2;
+
+    int sum, n = 0;
+
+    while (n <= size)
+    {
+        
+    }
+    
+    
+    
+    return 0;
+}
