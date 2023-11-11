71e427075d75fe10fe41a535fc4ddbd839cc6fef
Prateek <workspace.infoga@gmail.com>
2023-11-12 00:00:18 +0530

    DSA

--- cw/Day033/p04.cpp
+++ cw/Day033/p04.cpp
@@ -4,17 +4,23 @@
 using namespace std;
 int main()
 {
-    vector<int> arr = {1,2,3,4,5,6,8,9};
-    int size = arr.size();
+    vector<int> arr{1,2,3,4,5,6,7,9};
+    int size = arr.size() - 1;
     int actualSum = ((arr[size]) * (arr[size] + 1))/2;
 
-    int sum, n = 0;
+    int sum = 0;
 
-    while (n <= size)
+    for (int i = 0; i < 8; i++)
     {
-        
+    
+        sum = sum + arr[i];
+
     }
     
+    int missingNum = actualSum - sum;
+
+
+    cout << "Missing Number is : " << missingNum << endl;
     
     
     return 0;
