74b154d2a5fbf48f81343579289799335522324e
Prateek <workspace.infoga@gmail.com>
2023-11-20 21:30:17 +0530

    DSA

--- cw/Day038/p01.cpp
+++ cw/Day038/p01.cpp
@@ -1,10 +1,37 @@
+/**
+ * This C++ program finds the missing number in a given vector of integers.
+ * 
+ * @return The program is returning 0.
+ */
+
+// Alternate Ways for finding the missing number 
+
+// Method : 1:---
+
+// since for a continuos series the numbers start from 1 but the index starts from 0
+// so number = index + 1;
 
 #include<iostream>
 #include<vector>
 using namespace std;
 int main()
 {
+    vector<int> arr{1,2,3,4,5,6,7,9};
+    int missingNum = 0;
+    for (int i = 0; i < arr.size(); i++)
+    {
     
+        if (arr[i] - i != 1)
+        {
+            break;
+        }
+        
+
+        missingNum = arr[i] + 1;
+
+    }
+
+    cout << "Missing Number is : " << missingNum << endl;
     
     
     return 0;
