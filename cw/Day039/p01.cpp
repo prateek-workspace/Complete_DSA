c6ccdd73ba3dcaeac83018adb0022963986f696e
Prateek <workspace.infoga@gmail.com>
2023-11-21 21:57:20 +0530

    DSA

--- cw/Day039/p01.cpp
+++ cw/Day039/p01.cpp
@@ -2,10 +2,28 @@
 #include<iostream>
 #include<vector>
 using namespace std;
+int peakElement(vector<int> arr)
+{
+
+    int s = 0;
+    int e = arr.size() - 1;
+    int mid = s + (e-s)/2;
+    while(s<e)
+    {
+        if (arr[mid]<arr[mid+1])
+        {
+            s = mid + 1;
+        }
+        else
+        {
+            e = mid;
+        }
+    mid = s + (e-s)/2;
+    }
+    cout << e;
+}
 int main()
 {
-    
-    
-    
-    return 0;
+    vector<int> arr{0,10,5,2};
+    return peakElement(arr);    
 }
