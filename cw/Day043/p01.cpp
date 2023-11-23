4f7f58ed8add6749f237c19889e844236ce59a2b
Prateek <workspace.infoga@gmail.com>
2023-11-23 23:36:56 +0530

    DSA

---
+++ cw/Day043/p01.cpp
@@ -0,0 +1,38 @@
+// sort colors LeetCode - 75
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+    
+    vector<int> arr{1,1,0,2,0,1,2,0,1};
+    int least, mid= 0;
+    int high = arr.size() - 1; 
+    while (mid<=high)
+    {
+        if (arr[mid]==0)
+        {
+            swap(arr[mid], arr[least]);
+            least++;
+            mid++;
+        }
+        else if (arr[mid]==1)
+        {
+            mid++;
+        }
+        else if (arr[mid]==2)
+        {
+            swap(arr[mid], arr[high]);
+            high--;
+        }
+    }
+
+    // printing the array
+
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cout << arr[i] << ",";
+    }
+    
+    return 0;
+}
