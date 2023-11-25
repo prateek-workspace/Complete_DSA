4ac5fe037e554d8d7185933d1d5925d588486877
Prateek <workspace.infoga@gmail.com>
2023-11-25 21:06:07 +0530

    DSA

---
+++ cw/Day044/p02.cpp
@@ -0,0 +1,20 @@
+// Leetcode 287
+// Find Duplicate Number
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+    
+    vector<int> arr{1,3,4,5,6,7,8,9,0,0};
+    while (arr[0]!=arr[arr[0]])
+    {
+        swap(arr[0],arr[arr[0]]);
+    }
+        // printing the duplicate number
+
+        cout << "Duplicate number is : " << arr[0]<<endl;
+
+    
+    return 0;
+}
