aac81dbdb040047d672dad8eaf3232787b78eac7
Prateek <workspace.infoga@gmail.com>
2023-12-03 00:49:18 +0530

    DSA

--- cw/Day52/p01.cpp
+++ cw/Day52/p01.cpp
@@ -1,1 +1,45 @@
 // Factorial of a larger number.
+
+
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+    
+    int num;
+    cout << "Enter Number : " << endl;
+    cin >> num;
+
+    vector<int> ans;
+    ans.push_back(1);
+
+    int carry = 0;
+
+    for (int i = 2; i <= num; i++)
+    {
+        for (int j = 0; j < ans.size(); j++)
+        {
+            int temp =  ans[j]*i+carry;
+            ans[j] = (temp) % 10;
+            int carry = temp / 10;
+        }
+
+        if (carry)
+        {
+            ans.push_back(carry);
+        }
+        carry = 0;
+
+    }
+
+    reverse(ans.begin(), ans.end());
+
+    for (int i = 0; i < ans.size(); i++)
+    {
+        cout << ans[i] << " ";
+    }
+    
+    
+    return 0;
+}
