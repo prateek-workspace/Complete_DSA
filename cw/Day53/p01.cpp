5aabfefeac24240040e46710cca081512d367448
Prateek <workspace.infoga@gmail.com>
2023-12-03 22:53:57 +0530

    DSA

--- cw/Day53/p01.cpp
+++ cw/Day53/p01.cpp
@@ -1,20 +1,22 @@
 /**
- * The above C++ code finds the square root of a given number using binary search.
+ * The above code is a C++ program that calculates the square root of a given number using the binary
+ * search algorithm.
  * 
- * @param num The variable `num` represents the number for which you want to find the square root.
+ * @param num The variable `num` is the number for which you want to find the square root.
  * 
- * @return The function `findSqrt` returns the square root of the input number `num`.
+ * @return The function findSqrt() returns the square root of the input number.
  */
+
 
 #include<iostream>
 #include<vector>
 using namespace std;
-int findSqrt(int num){
-    int target = num;
+float findSqrt(float num){
+    float target = num;
     int start = 0;
     int end = num;
     int mid = start + (end-start)/2;
-    int ans;
+    float ans;
     while (start<=end)
     {
         if (mid*mid>target)
@@ -28,21 +30,60 @@ int findSqrt(int num){
         }
         if (mid*mid==target)
         {
-            return mid;
+            ans = mid;
         }
         
         mid = start + (end-start)/2;
 
     }
-        return ans;
+        // return ans;
+
+    // finding 0.1 position
+    float newAns;
+    float ans2;
+    float i = 0.1;
+    while (i<1.0)
+    {
+        newAns = ans+i;
+        if (newAns*newAns<target)
+        {
+            ans2 = newAns;
+        } 
+        if (newAns*newAns>target)
+        {
+            break;
+        }      
+        i = i + 0.1;
+    }
+    // return ans2;
+    
+    // finding 0.01 position
+    float newAns2;
+    float ans3;
+    float j = 0.01;
+    while (j<0.10)
+    {
+        newAns2 = ans2+j;
+        if (newAns2*newAns2<target)
+        {
+            ans3 = newAns2;
+        } 
+        if (newAns2*newAns2>target)
+        {
+            break;
+        }      
+        j = j + 0.01;
+    }
+    return ans3;
+
 }
 int main()
 {
-    int num;
+    float num;
     cout << "Enter no. you want square root of " << endl;
     cin >> num;
 
-    int ans = findSqrt(num);
+    float ans = findSqrt(num);
     
     cout << "sqrt is : " << ans << endl;
 
