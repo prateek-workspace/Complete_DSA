14dc3de0218ff5d13c8a4ddb64fba5a7b742094e
Prateek <workspace.infoga@gmail.com>
2023-12-03 23:07:42 +0530

    DSA

--- cw/Day53/p01.cpp
+++ cw/Day53/p01.cpp
@@ -1,13 +1,11 @@
 /**
- * The above code is a C++ program that calculates the square root of a given number using the binary
- * search algorithm.
+ * The above code is a C++ program that calculates the square root of a given number with a specified
+ * precision.
  * 
- * @param num The variable `num` is the number for which you want to find the square root.
+ * @param num The number for which you want to find the square root.
  * 
- * @return The function findSqrt() returns the square root of the input number.
+ * @return The function `findSqrt` returns the square root of the input number as a float.
  */
-
-
 #include<iostream>
 #include<vector>
 using namespace std;
@@ -36,45 +34,47 @@ float findSqrt(float num){
         mid = start + (end-start)/2;
 
     }
-        // return ans;
+        return ans;
+
+        // my way --> brute force ans temporary limited precison.
 
     // finding 0.1 position
-    float newAns;
-    float ans2;
-    float i = 0.1;
-    while (i<1.0)
-    {
-        newAns = ans+i;
-        if (newAns*newAns<target)
-        {
-            ans2 = newAns;
-        } 
-        if (newAns*newAns>target)
-        {
-            break;
-        }      
-        i = i + 0.1;
-    }
+    // float newAns;
+    // float ans2;
+    // float i = 0.1;
+    // while (i<1.0)
+    // {
+    //     newAns = ans+i;
+    //     if (newAns*newAns<target)
+    //     {
+    //         ans2 = newAns;
+    //     } 
+    //     if (newAns*newAns>target)
+    //     {
+    //         break;
+    //     }      
+    //     i = i + 0.1;
+    // }
     // return ans2;
     
     // finding 0.01 position
-    float newAns2;
-    float ans3;
-    float j = 0.01;
-    while (j<0.10)
-    {
-        newAns2 = ans2+j;
-        if (newAns2*newAns2<target)
-        {
-            ans3 = newAns2;
-        } 
-        if (newAns2*newAns2>target)
-        {
-            break;
-        }      
-        j = j + 0.01;
-    }
-    return ans3;
+    // float newAns2;
+    // float ans3;
+    // float j = 0.01;
+    // while (j<0.10)
+    // {
+    //     newAns2 = ans2+j;
+    //     if (newAns2*newAns2<target)
+    //     {
+    //         ans3 = newAns2;
+    //     } 
+    //     if (newAns2*newAns2>target)
+    //     {
+    //         break;
+    //     }      
+    //     j = j + 0.01;
+    // }
+    // return ans3;
 
 }
 int main()
@@ -84,8 +84,26 @@ int main()
     cin >> num;
 
     float ans = findSqrt(num);
+
+    // efficient way --> automated precision.
+
+    double finalAns = ans;
+    int precision;
+    cout << "Enter no of decimals upto which you want precision : ";
+    cin >> precision;
+
+    long double step = 0.1;
+    for (int i = 0; i < precision; i++)
+    {
+        for (long double j = finalAns; j*j <= num; j = j+step)
+        {
+            finalAns = j;
+        }
+
+        step = step/10;
+    }
     
-    cout << "sqrt is : " << ans << endl;
+    cout << "Square Root is : " << finalAns << endl;
 
     return 0;
 }
