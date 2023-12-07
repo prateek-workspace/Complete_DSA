4165f2119c5e39835f11b91249575f9aee2e0f10
Prateek <workspace.infoga@gmail.com>
2023-12-07 22:52:39 +0530

    DSA

---
+++ cw/Day056/p01.cpp
@@ -0,0 +1,74 @@
+// division of two numbers using binary search with precision.
+#include<iostream>
+#include<vector>
+using namespace std;
+float division(int divisor, int dividend )
+{
+
+    int start = 0;
+    int end = dividend;
+    int mid = start + (end - start)/2;
+    float ans;
+
+    while (start <= end)
+    {
+        if (abs(mid*divisor)==abs(dividend))
+        {   
+            ans = mid;
+            end = mid -1;
+        }
+        else if (abs(mid*divisor)>abs(dividend))
+        {
+            end = mid - 1;
+        }
+        else if (abs(mid*divisor)<abs(dividend))
+        {
+            start = mid + 1;
+            ans = mid;
+        }  
+
+        mid = start + ((end-start)/2); 
+
+    }
+    // return ans;
+
+    
+    float finalAns = ans;
+    int precision;
+    cout << "Enter no of decimals upto which you want precision : ";
+    cin >> precision;
+
+    float step = 0.1;
+    for (float i = 0; i < precision; i++)
+    {
+        for (float j = 0; divisor*j <= dividend; j = j+step)
+        {
+            finalAns = j;
+        }
+
+        step = step/10;
+    }
+
+    if (divisor>0 && dividend>0 || divisor<0 && dividend<0)
+    {
+        finalAns = finalAns*1;
+    }
+    else{
+        finalAns = finalAns*-1;
+    }
+    
+    cout << "Answer is : " << finalAns << endl;
+
+}
+int main()
+{
+    float divisor,dividend;
+    cout << "Enter the Dividend : " << endl;
+    cin >> dividend;
+    cout << "Enter the Divisor : " << endl;
+    cin >> divisor;
+    division(divisor,dividend);
+
+
+    return 0;
+}
---
+++ cw/Day057/p01.cpp
@@ -0,0 +1,31 @@
+// oddly occuring element.
+
+// using hashmap.
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+using namespace std;
+int main()
+{
+    
+    int arr[] = {1,1,2,2,3,3,4,5,4,6,5};
+    int size = sizeof(arr)/sizeof(int);
+    unordered_map<int,int> hash;
+    
+    for (int i = 0; i < size; i++)
+    {
+        hash[arr[i]]++;
+    }
+    
+    for (int i = 0; i < size; i++)
+    {
+        if (hash[arr[i]]%2!=0)
+        {
+            cout << "Oddly occuring element is : " << arr[i] << endl;
+            break;
+        }
+        
+    }
+    
+    return 0;
+}
