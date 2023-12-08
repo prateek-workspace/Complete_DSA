00a6bda5b8cad4f2d54d0a68d09f946407c40886
Prateek <workspace.infoga@gmail.com>
2023-12-08 21:14:55 +0530

    DSA

--- cw/Day058/p01.cpp
+++ cw/Day058/p01.cpp
@@ -1,22 +1,48 @@
 // oddly repeating number.
+
 // Solved using XOR opeartor.
 // Time Complexity --> O(n) & Space Complexity --> O(n).
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+// int main()
+// {
+    
+//     int arr[] = {1,4,4,2,2,4,4,3,3,5,5};
+//     int size = sizeof(arr)/sizeof(int);
+//     int ans = 0;
+
+//     for (int i = 0; i < size; i++)
+//     {
+//         ans = arr[i]^ans;  
+//     }
+
+//     cout << "Oddly occuring element is : " << ans << endl;
+     
+//     return 0;
+// }
+
+
+// Using Indexing.
+
+// Time Complexity --> O(n), Space Complexity --> O(1).
+
 #include<iostream>
 #include<vector>
 using namespace std;
 int main()
 {
-    
-    int arr[] = {1,1,4,4,2,2,4,4,3,3,5,5,3};
+
+    int arr[] = {1,1,4,4,2,2,4,4,2};
     int size = sizeof(arr)/sizeof(int);
-    int ans = 0;
-
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i <= size; i++)
     {
-        ans = arr[i]^ans;  
+        if (i%2!=0 && arr[i]==arr[i+1])
+        {
+            cout << "Oddly occuring element is : " << arr[i-1] << endl; 
+            break;          
+        }  
     }
-
-    cout << "Oddly occuring element is : " << ans << endl;
-     
+    
     return 0;
 }
