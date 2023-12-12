ec8d1d8c1e8013dd87be5de4e6cebe92c704f6de
Prateek <workspace.infoga@gmail.com>
2023-12-12 07:54:20 +0530

    DSA

---
+++ cw/Day061/p01.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int countKDifference(vector<int>& nums, int k) {
+        int count = 0;
+        for(int i = 0 ; i < nums.size(); i++)
+        {
+            for(int j = i + 1 ; j < nums.size(); j++)
+            {
+                if(nums[i] - nums[j] == k || nums[j] - nums[i] == k)
+                {
+                   count++;
+                }
+            }
+        }
+        return count;
+    }
+};
