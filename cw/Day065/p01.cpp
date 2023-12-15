6ef4b351332bcd3485ac8360cd889817514273fd
Prateek <workspace.infoga@gmail.com>
2023-12-15 22:37:07 +0530

    DSA

--- cw/Day065/p01.cpp
+++ cw/Day065/p01.cpp
@@ -4,8 +4,28 @@
 using namespace std;
 int main()
 {
-    
-    
-    
+    vector<int> arr = {19,12,10,11,4,5};
+    int size = arr.size();
+    // traversing the array
+    for (int i = 0; i < arr.size()-1; i++)
+    {
+
+        // finding smaaler element in the remaining array.
+
+        for (int j = i+1; j < arr.size(); j++)
+        {
+            if (arr[j]<arr[i])
+            {
+                swap(arr[i],arr[j]);
+            }            
+        }
+    }
+
+    // printing the array.
+
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cout << arr[i] << " ";
+    }
     return 0;
 }
