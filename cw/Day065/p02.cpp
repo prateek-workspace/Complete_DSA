02a6596f53cb1699af36be3879a87d1f99249267
Prateek <workspace.infoga@gmail.com>
2023-12-16 23:04:22 +0530

    DSA

--- cw/Day065/p02.cpp
+++ cw/Day065/p02.cpp
@@ -7,16 +7,32 @@ int main()
     vector<int> arr = {19,12,10,11,4,5};
     int size = arr.size();
     // traversing the array --> representing the rounds as there is no need to check for last element(greatest one) so range is 0 to (n-1).
-    for (int i = 0; i < arr.size()-1; i++)
+
+    
+    // for (int i = 0; i < arr.size()-1; i++)
+    // {
+
+    //     // finding larger element in the remaining array.
+
+    //     for (int j = 0; j < size - i - 1; j++)
+    //     {
+    //         if (arr[j]>arr[j+1])
+    //         {
+    //             swap(arr[j],arr[j+1]);
+    //         }            
+    //     }
+    // }
+
+    for (int i = 1; i < arr.size(); i++)
     {
 
         // finding larger element in the remaining array.
 
-        for (int j = i; j < size; j++)
+        for (int j = 0; j < size - i; j++)
         {
-            if (arr[i]>arr[j])
+            if (arr[j]>arr[j+1])
             {
-                swap(arr[i],arr[j]);
+                swap(arr[j],arr[j+1]);
             }            
         }
     }
