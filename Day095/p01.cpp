7a8d7946c878acb823822d2797d3c08272bcc6dd
Prateek <workspace.infoga@gmail.com>
2024-01-27 23:32:40 +0530

    DSA

---
+++ Day095/p01.cpp
@@ -0,0 +1,24 @@
+/* The Solution class provides a method to find the largest number that can be formed by concatenating
+the elements of a given vector of integers. */
+class Solution {
+public:
+    static bool mycomp(string a, string b) {
+        string t1 = a + b;
+        string t2 = b + a;
+        return t1 > t2;
+    }
+    string largestNumber(vector<int>& nums) {
+        vector<string> snums;
+        for (auto n : nums) {
+            snums.push_back(to_string(n));
+        }
+        sort(snums.begin(), snums.end(), mycomp);
+        if (snums[0] == "0")
+            return "0";
+        string ans = "";
+        for (auto str : snums) {
+            ans += str;
+        }
+        return ans;
+    }
+};
