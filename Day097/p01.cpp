daba7aae26a3ff94f0709bf4580084b36d073daa
Prateek <workspace.infoga@gmail.com>
2024-01-29 00:51:45 +0530

    DSA

---
+++ Day097/p01.cpp
@@ -0,0 +1,40 @@
+/* The Solution class contains a method called convert that takes a string and an integer as input and
+returns a modified version of the string based on a zigzag pattern. */
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        if (numRows == 1) {
+            return s;
+        }
+
+        vector<string> zigzag(numRows);
+
+        int i = 0;
+        int row = 0;
+        bool direction = 1;
+
+        while (true) {
+            if (direction) {
+                while (row < numRows && i < s.size()) {
+                    zigzag[row++].push_back(s[i++]);
+                }
+                row = numRows - 2;
+            } else {
+                while (row >= 0 && i < s.size()) {
+                    zigzag[row--].push_back(s[i++]);
+                }
+                row = 1;
+            }
+            if (i >= s.size()) {
+                break;
+            }
+            direction = !direction;
+        }
+        string ans = "";
+        for (int i = 0; i < zigzag.size(); i++) {
+            ans += zigzag[i];
+            cout << zigzag[i] << endl;
+        }
+        return ans;
+    }
+};
