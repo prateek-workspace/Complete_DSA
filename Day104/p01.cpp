f6747ccbb206b4582ae4ea7196dd94092bbf52f0
Prateek Srivastava <110523559+prateek-workspace@users.noreply.github.com>
2024-02-08 21:11:15 +0530

    DSA

--- Day104/p01.cpp
+++ Day104/p01.cpp
@@ -18,28 +18,3 @@ public:
         return count;
     }
 };
-
-class Solution {
-public:
-    bool isPrime(int n) {
-        if(n<=1){
-        return false;
-}
-int sqrtN = sqrt(n);
-        for (int i = 2; i <=sqrtN; i++) {
-            if (n % i == 0) {
-                return false;
-            }
-        }
-        return true;
-    }
-    int countPrimes(int n) {
-        int count = 0;
-        for (int i = 2; i < n; i++) {
-            if (isPrime(i)) {
-                ++count;
-            }
-        }
-        return count;
-    }
-};
