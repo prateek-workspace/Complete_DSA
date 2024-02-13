ff31628a6ddf180db18d19d2757f75581d036e3b
Prateek Srivastava <110523559+prateek-workspace@users.noreply.github.com>
2024-02-14 02:49:51 +0530

    DSA

--- Day108/p01.cpp
+++ Day108/p01.cpp
@@ -2,8 +2,16 @@
 using namespace std;
 
 void util(int *p){
-//   her value is pass by value so in this a copy of p is created and that is also pointing towards a so updating p doesn't have any effect on the code
+//   here value is pass by value so in this a copy of p is created and that is also pointing towards a so updating p doesn't have any effect on the code
   p = p + 1;
+}
+
+void util2(int *p){
+//   here value is pass by value so in this a copy of p is created and that is also pointing towards a.
+
+// but here we are not updating p but we are going to update the value to which p is pointing bcs=z we are updating *p
+
+  *p = *p + 1;
 }
 
 int main() {
@@ -13,12 +21,19 @@ int main() {
 
   cout << "before" << endl;
   cout << a << endl;
-  cout << p << endl;
+  cout << p << endl;      
   cout << *p << endl;
 
   util(p);
 
-  cout << "after" << endl;
+  cout << "after util " << endl;
+  cout << a << endl;
+  cout << p << endl;
+  cout << *p << endl;
+
+  util2(p);
+
+  cout << "after util 2 " << endl;
   cout << a << endl;
   cout << p << endl;
   cout << *p << endl;
